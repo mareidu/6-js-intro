1047ecaa2c4c655ebe3c3834a01f6985bc58ec09
mareidu <79446370+mareidu@users.noreply.github.com>
2021-03-09 23:56:18 +0200

    pirmas

---
+++ js/nd1-2.js
@@ -0,0 +1,29 @@
+const randsk1 = 5;
+console.log(randsk1);
+
+const randsk2 = 15;
+console.log(randsk2);
+
+const randsk3 = 25;
+console.log(randsk3);
+
+const vardpava = 'Vardas ir pavarde';
+console.log(vardpava);
+
+const amzius = 'Amzius';
+console.log(amzius);
+
+const elpastas = 'El. pastas';
+console.log(elpastas);
+
+const msv1 = [4, 8, 15, 18, 36];
+console.log(msv1);
+
+const msv2 = [3, 7, 16, 75, -43];
+console.log(msv2);
+
+const msv3 = [3, 8, 45.90, 54.32, 12.56];
+console.log(msv3);
+
+const msv4 = ['Darzelis', 'Mokykla', 'Kolegija', 'Profesine', 'Universitetas'];
+console.log(msv4);
