3d9cc966eed6879d286755cc274e92f648bb8c84
mareidu <79446370+mareidu@users.noreply.github.com>
2021-03-09 23:34:33 +0200

    nice

---
+++ js/01-variables.js
@@ -0,0 +1,31 @@
+const vardas = 'Marija';
+console.log(vardas);
+
+const pasisv = 'Labas rytas, Lietuva!';
+console.log(pasisv);
+
+const pirmasSkaicius = 7;
+const antrasSkaicius = 77;
+const sum = pirmasSkaicius + antrasSkaicius;
+
+console.log(pirmasSkaicius);
+console.log(antrasSkaicius);
+const suma = pirmasSkaicius + antrasSkaicius;
+
+console.log(pirmasSkaicius);
+console.log(antrasSkaicius);
+console.log(suma);
+
+console.log(pirmasSkaicius, '+', antrasSkaicius, '=', suma);
+
+const petrasName = 'Petras';
+const age = 99;
+
+const petrasAge = petrasName + ' age is ' + age + '.';
+console.log(petrasAge);
+
+const pazymiai = [6,8, 10, 5, 6, 8];
+console.log(pazymiai);
+
+const abc = ['a', 'b', 'd', 'e', 'g'];
+console.log(abc);
