fbdc6f6f2b6ae2392eef1f3b7f952a9ca01e515d
mareidu <79446370+mareidu@users.noreply.github.com>
2021-03-10 00:12:03 +0200

    antras

--- js/nd1-2.js
+++ js/nd1-2.js
@@ -27,3 +27,32 @@ console.log(msv3);
 
 const msv4 = ['Darzelis', 'Mokykla', 'Kolegija', 'Profesine', 'Universitetas'];
 console.log(msv4);
+
+const msv5 = ['Mano', 'batai buvo', 'du', 'vienas dingo', 'nerandu'];
+console.log(msv5);
+
+const msv6 = ['kraujas', 'limfa', 'kaulai', 'organai', 'raumenys'];
+console.log(msv6);
+
+console.log('*****************');
+
+const suma = randsk1 + randsk2 + randsk3;
+console.log(suma);
+
+const rink = `${vardpava} ${amzius} ${elpastas}`;
+console.log(rink);
+//1-2+3-4+5
+let amount1 = 0;
+amount1 =  amount1 + msv3[0];
+amount1 = amount1 - msv3[1];
+amount1 = amount1 + msv3[2];
+amount1 = amount1 - msv3[3];
+amount1 = amount1 + msv3[4];
+console.log(amount1);
+
+let eiles = '';
+eiles = eiles + ', ' + msv5[4];
+eiles = eiles + ', ' + msv5[3];
+eiles = eiles + ', ' + msv5[2];
+eiles = eiles + ', ' + msv5[1];
+console.log(eiles);
