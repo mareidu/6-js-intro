8965609582a8a18e405189cce01cc99df8d05382
mareidu <79446370+mareidu@users.noreply.github.com>
2021-03-25 10:18:32 +0200

    penkta

---
+++ js/05-ternary.js
@@ -0,0 +1,19 @@
+const a = 5;
+const b = 7;
+
+let ats = '';
+
+if (a > b) {
+    ats = 'daugiau';
+} else (
+    ats = 'lygus arba maziau';
+)
+
+console.log(ats);
+
+const c = 5;
+const d = 7;
+
+const ats2 = c > d ? 'daugiau' : 'lygus arba maziau';
+
+console.log(ats2);
