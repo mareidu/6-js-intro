b9a121227cdc723710603638583ff106c375528f
mareidu <79446370+mareidu@users.noreply.github.com>
2021-04-26 23:08:17 +0300

    aštuntas

---
+++ js/08-sum.js
@@ -0,0 +1,16 @@
+const start = 0;
+const n = 10;
+let suma = 0;
+
+if (start <= iki) {
+    for (let i = start <= iki; i++) {
+        suma += i;
+    }
+} else {
+    for (let i = iki; i <= nuo; i++) {
+        suma += i;
+    }
+}
+
+const ats = `Intervale nuo ${nuo} iki ${iki} suma yra ${suma}.`;
+console.log(ats);
