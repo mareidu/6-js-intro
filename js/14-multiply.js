c73250e562fddbdc89acf0fa936f9a6f4e5be0e1
mareidu <79446370+mareidu@users.noreply.github.com>
2021-04-27 07:17:42 +0300

    product

---
+++ js/14-multiply.js
@@ -0,0 +1,9 @@
+function daugyba(a, b) {
+    const rez = a * b;
+    return rez;
+}
+
+console.log(daugyba(4, 5));
+console.log(daugyba(14, 54));
+console.log(daugyba(-3, 23));
+console.log(daugyba(3.14, 590));
